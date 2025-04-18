1a90af3463f29559c32fe9fee0f2c1f41b8aad47
Sourav01112 <chaudharysourav997@gmail.com>
2025-04-19 00:18:16 +0530

    script change

--- k8s/kube-dns-node.sh
+++ k8s/kube-dns-node.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-SERVER=$(grep 'server:' ~/.kube/config | awk '{print $2}')
+KUBECONFIG_PATH="${KUBECONFIG:-$HOME/.kube/config}"
+
+if [ ! -f "$KUBECONFIG_PATH" ]; then
+  echo "❌ kubeconfig not found at $KUBECONFIG_PATH"
+  exit 1
+fi
+
+SERVER=$(grep 'server:' "$KUBECONFIG_PATH" | awk '{print $2}')
 PORT=$(echo "$SERVER" | grep -oE '[0-9]+$')
 
 if ping -c 1 -W 1 host.docker.internal &>/dev/null; then
@@ -11,9 +18,29 @@ else
   NEW_HOST="127.0.0.1"
 fi
 
-sed -i '' "s|server: .*|server: https://$NEW_HOST:$PORT|" ~/.kube/config
+echo "🔧 Updating kubeconfig with host $NEW_HOST and port $PORT"
+sed -i'' "s|server: .*|server: https://$NEW_HOST:$PORT|" "$KUBECONFIG_PATH"
 
-kubectl "$@"
+kubectl config view --minify | grep server
+
+
+
+# #!/bin/bash
+
+# SERVER=$(grep 'server:' ~/.kube/config | awk '{print $2}')
+# PORT=$(echo "$SERVER" | grep -oE '[0-9]+$')
+
+# if ping -c 1 -W 1 host.docker.internal &>/dev/null; then
+#   echo "👉 Inside Docker/Jenkins - using host.docker.internal"
+#   NEW_HOST="host.docker.internal"
+# else
+#   echo "✅ On Mac host - using 127.0.0.1"
+#   NEW_HOST="127.0.0.1"
+# fi
+
+# sed -i '' "s|server: .*|server: https://$NEW_HOST:$PORT|" ~/.kube/config
+
+# kubectl "$@"
 
 
 
