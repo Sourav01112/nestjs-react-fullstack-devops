bd1b2af4995c1878d411c90374c85c36848b2ea9
Sourav01112 <chaudharysourav997@gmail.com>
2025-04-19 01:04:16 +0530

    script change-6

--- k8s/k8s-apply.sh
+++ k8s/k8s-apply.sh
@@ -5,10 +5,10 @@ echo "📦 Contents:"
 ls -la
 
 
-# kubectl --insecure-skip-tls-verify=true apply -f namespace.yml
-# kubectl --insecure-skip-tls-verify=true apply -f secrets/backend-secrets.yml
-# kubectl --insecure-skip-tls-verify=true apply -f config/
-# kubectl --insecure-skip-tls-verify=true apply -f backend/
+kubectl apply -f namespace.yml
+kubectl apply -f secrets/backend-secrets.yml
+kubectl apply -f config/
+kubectl apply -f backend/
 
 
 # kubectl apply -f ./namespace.yml
