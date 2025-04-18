b60276935c8f041e55a338d12db2d402b8861ed3
Sourav01112 <chaudharysourav997@gmail.com>
2025-04-19 00:43:45 +0530

    added skip tls=true

--- k8s/k8s-apply.sh
+++ k8s/k8s-apply.sh
@@ -1,7 +1,7 @@
-kubectl apply -f ./namespace.yml
-kubectl apply -f ./secrets/backend-secrets.yml
-kubectl apply -f ./config/
-kubectl apply -f ./backend/
+kubectl --insecure-skip-tls-verify=true apply -f ./namespace.yml
+kubectl --insecure-skip-tls-verify=true apply -f ./secrets/backend-secrets.yml
+kubectl --insecure-skip-tls-verify=true apply -f ./config/
+kubectl --insecure-skip-tls-verify=true apply -f ./backend/
 # kubectl apply -f k8s/frontend/
 
 
