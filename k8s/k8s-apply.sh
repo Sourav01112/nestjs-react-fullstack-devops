bff1141cffa02695cdb0d914bd91470194df284e
Sourav01112 <chaudharysourav997@gmail.com>
2025-04-19 00:58:44 +0530

    script change-3

--- k8s/k8s-apply.sh
+++ k8s/k8s-apply.sh
@@ -1,7 +1,17 @@
-kubectl --insecure-skip-tls-verify=true apply -f ./namespace.yml
-kubectl --insecure-skip-tls-verify=true apply -f ./secrets/backend-secrets.yml
-kubectl --insecure-skip-tls-verify=true apply -f ./config/
-kubectl --insecure-skip-tls-verify=true apply -f ./backend/
+#!/bin/bash
+set -e
+cd "$(dirname "$0")"# Ensures script always runs from its own directory
+
+kubectl --insecure-skip-tls-verify=true apply -f namespace.yml
+kubectl --insecure-skip-tls-verify=true apply -f secrets/backend-secrets.yml
+kubectl --insecure-skip-tls-verify=true apply -f config/
+kubectl --insecure-skip-tls-verify=true apply -f backend/
+
+
+# kubectl apply -f ./namespace.yml
+# kubectl apply -f ./secrets/backend-secrets.yml
+# kubectl apply -f ./config/
+# kubectl apply -f ./backend/
 # kubectl apply -f k8s/frontend/
 
 
